ba4a955f07782a9c88faeed1c42b967f370a33b2
shibabu <shibabu.alfa@gmail.com>
2015-05-08 01:51:53 +0900

    Initial commit: Generated Pin Model, Devise views and pins controller

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,25 @@
+class PinsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def vote
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
